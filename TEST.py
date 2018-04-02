be4bf3fa33c9f52f11029a9ddfc40750b730afd8
Jie Tao <taoj@mail.gvsu.edu>
2018-04-02 01:15:27 -0400

    0.61
    add config.ini file

--- TEST.py
+++ TEST.py
@@ -1,6 +1,11 @@
 import cv2
 import urllib.request
 import numpy as np
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+print(config["default"]["y_file"])
 
 req = urllib.request.urlopen('http://www.studiodentaire.com/images/smile-man-open-mouth.jpg')
 arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
